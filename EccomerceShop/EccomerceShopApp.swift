703575bc1b6a3aa208f768b46d4850a4d4e962f8
Hüseyin Umut Kardaş <kardasumt@gmail.com>
2024-11-15 23:42:24 +0300

    Initial Commit

---
+++ EccomerceShop/EccomerceShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  EccomerceShopApp.swift
+//  EccomerceShop
+//
+//  Created by Hüseyin Umut Kardaş on 15.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct EccomerceShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
