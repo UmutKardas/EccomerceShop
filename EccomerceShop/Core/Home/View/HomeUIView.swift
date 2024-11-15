54d876e36cb7dfdb1f11c264d2823c89c56920f8
Hüseyin Umut Kardaş <kardasumt@gmail.com>
2024-11-16 00:09:47 +0300

    Added required colors and assets to the project

---
+++ EccomerceShop/Core/Home/View/HomeUIView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeUIView.swift
+//  EccomerceShop
+//
+//  Created by Hüseyin Umut Kardaş on 15.11.2024.
+//
+
+import SwiftUI
+
+struct HomeUIView: View {
+    var body: some View {
+        ZStack {
+            Color("backgroundColor")
+                .ignoresSafeArea()
+        }
+    }
+}
+
+#Preview {
+    HomeUIView()
+}
